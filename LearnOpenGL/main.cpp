feba08aaae837b62ca46e6f44a91ba4f7f51b547
Kye <kyenelson3@gmail.com>
2022-04-11 21:56:21 -0700

    Added comments for learning.

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -16,6 +16,7 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	//create GLFW object. Inlcuding Error checking.
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 	if (window == NULL)
 	{
@@ -23,7 +24,9 @@ int main()
 		glfwTerminate();
 		return -1;
 	}
+	//use the object window as the current window.
 	glfwMakeContextCurrent(window);
+	//function to change window size when dragging window.
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -31,11 +34,17 @@ int main()
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
+	//set viewport inside the window
 	glViewport(0, 0, 800, 600);
 
+	//render loop
 	while(!glfwWindowShouldClose(window))
 	{
+		//front render is what you see on screen. once the back render 
+		//finishes rendering it swaps with the front render. this avoids 
+		//artifacts if using one buffer.
 		glfwSwapBuffers(window);
+		//checks for input events.
 		glfwPollEvents();
 	}
 
@@ -43,6 +52,8 @@ int main()
 	return 0;
 }
 
+//function that sets internal viewport of window to the window size when
+//resizing window.
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
