54351f0877ff6f78b99072821a003eb88f8b5e31
Kye <kyenelson3@gmail.com>
2022-04-12 21:47:16 -0700

    Day 2 (4.5 - 5.1): Added input, color buffers, and part of Vertex shader.

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -5,46 +5,92 @@
 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+const int SCR_WIDTH = 800;
+const int SCR_HEIGHT = 600;
+
 
 
 int main()
 {
-	//initializes glfw
+
+	//----------------------GLFW and GLAD initialization--------------------------
+	// Initializes glfw
 	glfwInit();
-	//tells glfw what version of opengl to use. this case: 4.6 Core
+	// Tells glfw what version of opengl to use. this case: 4.6 Core
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	//create GLFW object. Inlcuding Error checking.
-	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	// Create GLFW object. Inlcuding Error checking.
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
-	//use the object window as the current window.
+	// Use the object window as the current window.
 	glfwMakeContextCurrent(window);
-	//function to change window size when dragging window.
+	// Function to change window size when dragging window.
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	// glad just calls openGL functions in an easier way
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
-	//set viewport inside the window
-	glViewport(0, 0, 800, 600);
 
-	//render loop
+
+	//------------------------Main OpenGL Functions-------------------------------
+
+
+	// Set viewport inside the window
+	glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+
+
+
+	// Vertices for a triangle
+	float vertices[] =
+	{
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f
+	};
+	// glGenBuffers assigns 1 buffer object to VBO. 
+	// A buffer object represents memory allocated by OpenGL for the GPU.
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	// Binds the buffer. creates the buffer object. this activates object VBO.
+	// Now you can use or manipulate VBO.
+	// There can only be one object of each type active at a time. 
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+	
+
+
+	//------------------------------Render Loop-----------------------------------
 	while(!glfwWindowShouldClose(window))
 	{
-		//front render is what you see on screen. once the back render 
-		//finishes rendering it swaps with the front render. this avoids 
-		//artifacts if using one buffer.
+		// Input
+		processInput(window);
+
+		// Rendering commands
+
+		// Sets the color when the color buffer is cleared.
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		// Clears the color buffer.
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Front render is what you see on screen. once the back render 
+		// finishes rendering it swaps with the front render. this avoids 
+		// artifacts if using one buffer.
 		glfwSwapBuffers(window);
-		//checks for input events.
+		// Checks for input events.
 		glfwPollEvents();
 	}
 
@@ -52,9 +98,19 @@ int main()
 	return 0;
 }
 
-//function that sets internal viewport of window to the window size when
-//resizing window.
+// Sets OpenGL viewport size to GLFW window size when resizing.
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
+}
+
+
+// Input function to organize all the inputs.
+void processInput(GLFWwindow* window)
+{
+	// Escape to close window.
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
 }
